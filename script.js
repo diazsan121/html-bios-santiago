8c223bb9a1b4f477a348a2bae17c10a9f59853a3
santiago <102105748+diazsan121@users.noreply.github.com>
2024-07-29 20:00:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,63 @@
 
-/*function AgregarOnClick(){ 
-document.miFormulario.agregartarea.click(); 
-var msg1 = document.getElementById('HOJA1'); 
-var msg2 = document.getElementById('HOJA2');
-var msg3 = document.getElementById('HOJA3');    
-if (document.miFormulario.tarea1.value) {
- msg1.innerText = 'El elemento está marcado tarea1'; 
- lista.push(msg1);}
-else { msg1.innerText = ""}
-} */
-function Agregar(tarea){ 
-   
+var x=0;
+var tareas=[];
 
-        document.getElementById("HOJA1").innerHTML = tarea;
-
-document.getElementById("HOJA2").innerHTML = tarea;
-
-document.getElementById("HOJA3").innerHTML = tarea;
-
-
+function Agregar(tarea){  
+x=x+1;
+//creamos nuestra lista
+if (x==1) {   document.getElementById("HOJA1").innerHTML = tarea;
+   tareas.push(tarea);
+}
+     
+if (x==2) {    document.getElementById("HOJA2").innerHTML = tarea;  
+   tareas.push(tarea);
+ }
+if (x==3) {
+   document.getElementById("HOJA3").innerHTML = tarea;   
+   tareas.push(tarea);
+}
 } 
 function completar(){ 
     var msg1 = document.getElementById('HOJA1'); 
     var msg2 = document.getElementById('HOJA2');
     var msg3 = document.getElementById('HOJA3');  
-    var lista = [];
-
+          
     if (document.miFormulario.cbox1.checked) {
-        msg1.innerText = 'La tarea 1 está completada'; 
-        lista.push(msg1);}
+        msg1.innerText = 'La tarea 1 está completada'; }
      else { msg1.innerText = ""}
-      
-    
-       if (document.miFormulario.cbox2.checked) {
-        msg2.innerText = 'La tarea 2 está completada'; 
-        lista.push(msg2);}
-     else { msg2.innerText = ""}
+       
+     if (document.miFormulario.cbox2.checked) {
+        msg2.innerText = 'La tarea 2 está completada'; }
+      else { msg2.innerText = ""}
       
      if (document.miFormulario.cbox3.checked) {
-        msg3.innerText = 'La tarea 3 está completada'; 
-        lista.push(msg3);}
+        msg3.innerText = 'La tarea 3 está completada'; }
      else { msg3.innerText = ""}
       
-
+}
+function ver_lista(){
+   //recorre elemento por elemento y los muestra con alert()
+   for(i=0; i<tareas.length; i++) {
+      alert(tareas[i]);   
+}
 }
 
 function eliminar(){ 
+   tareas.pop();
     var msg1 = document.getElementById('HOJA1'); 
     var msg2 = document.getElementById('HOJA2');
     var msg3 = document.getElementById('HOJA3');  
-    var articulos = ["   t1","  t2","    T3"];
-    var lista = [];
-
-    if (document.miFormulario.cbox1.checked) {
-        msg1.innerText = ''; 
-        lista.push(msg1);}
-     else { msg1.innerText = ""}
-      
     
-       if (document.miFormulario.cbox2.checked) {
-        msg2.innerText = ''; 
-        lista.push(msg2);}
-     else { msg2.innerText = ""}
+    if (document.miFormulario.cbox1.checked) {
+        msg1.innerText = 'LA TAREA1 FUE ELIMINADA';  }        
+    else { msg1.innerText = ""}     
+    
+    if (document.miFormulario.cbox2.checked) {
+        msg2.innerText = 'LA TAREA2 FUE ELIMINADA'; }
+    else { msg2.innerText = ""}
       
-     if (document.miFormulario.cbox3.checked) {
-        msg3.innerText = ''; 
-        lista.push(msg3);}
-     else { msg3.innerText = ""}
-     
-     document.write(articulos[0]);    
-     document.write(articulos[1]); 
-     document.write(articulos[2]); 
-     document.write(lista[0]);    
-     document.write(lista[1]);      
+    if (document.miFormulario.cbox3.checked) {
+        msg3.innerText = 'LA TAREA3 FUE ELIMINADA'; }
+    else { msg3.innerText = ""}
+        
 }
